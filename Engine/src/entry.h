50108461a9d655d7bd23b6d73bd9d288dd02b504
Taha KARABIYIK <tahakrb275@gmail.com>
2024-04-20 23:07:20 +0300

    Memory Subsystem

--- Engine/src/entry.h
+++ Engine/src/entry.h
@@ -2,6 +2,7 @@
 
 #include "core/application.h"
 #include "core/logger.h"
+#include "core/kmemory.h"
 #include "game_types.h"
 
 // Externally-defined function to create a game
@@ -13,8 +14,9 @@ extern b8 create_game(game* out_game);
 
 int main(void)
 {
+    initialize_memory();
+
     // Request the game instance from the application
-    
     game game_inst;
     if(!create_game(&game_inst))
     {
@@ -43,5 +45,6 @@ int main(void)
         return 2;
     }
 
+    shutdown_memory();
     return 0;
 }
