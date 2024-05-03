1655102882812a022ffdfa9181c6b551b5f364a4
Taha KARABIYIK <tahakrb275@gmail.com>
2024-05-04 01:43:48 +0300

    Vulkan extensions, layers, debug

--- Engine/src/core/kstring.c
+++ Engine/src/core/kstring.c
@@ -16,3 +16,9 @@ char* string_duplicate(const char* str)
     return copy;
 
 }
+
+// Case-sensitive string comparison. True if the same, otherwise false.
+KAPI b8 strings_equal(const char* str0, const char* str1)
+{
+    return strcmp(str0, str1) == 0;
+}
